665b7d548ef70f5e0d8d4d02ace7ae896c329265
nithin.thomas <nithin.thomas@hitachivantara.com>
2021-05-30 19:00:52 +0530

    changes

--- cluster/istio_install.tf
+++ cluster/istio_install.tf
@@ -1,9 +1,8 @@
 resource "null_resource" "install_istio" {
   provisioner "local-exec" {
-    command = "istioctl install -f \"istio-operator.yaml\" --kubeconfig var.kubeconfig -y"
+    command = "istioctl install -f \"istio-operator.yaml\" --kubeconfig /root/.kube/config -y"
   }
-  depends_on = [
-      local_file.setup_istio_config,
+  depends_on = [   
       aws_eks_cluster.istio-cluster,
       aws_eks_node_group.demo
   ]
